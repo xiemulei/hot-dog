5a5ed459f2fa1278799a7ecb4fd6a4f6caa0f4e1
xiemulei <xiemulei@gmail.com>
2025-12-20 21:52:37 +0800

    添加狗狗图片展示，修改页面样式

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use dioxus::prelude::*;
+use dioxus::{logger::tracing, prelude::*};
+
+static CSS: Asset = asset!("/assets/main.css");
 
 fn main() {
     dioxus::launch(App);
@@ -6,5 +8,26 @@ fn main() {
 
 #[component]
 fn App() -> Element {
-    rsx! { "HotDog!" }
+    rsx! {
+        document::Stylesheet { href: CSS }
+        div { id: "title",
+            h1 { "HotDog!" }
+        }
+
+        div { id: "dogview",
+            img { src: "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg" }
+        }
+
+        div { id: "buttons",
+            button { id: "skip", "skip" }
+            button { id: "save", "save!" }
+        }
+    }
+}
+
+#[component]
+fn DogApp(breed: String) -> Element {
+    tracing::info!("Rendered with breed: {breed}");
+
+    todo!()
 }
